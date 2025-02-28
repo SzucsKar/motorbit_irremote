6e20fb39b4ab1c1a04315f12afa984f1b078f1f7
SzucsKar <67927917+SzucsKar@users.noreply.github.com>
2025-02-28 17:58:58 +0100

    main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -4,6 +4,6 @@ EM_IR.OnPressEvent(function (message) {
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
 })
-basic.showString("IR DEMO")
+basic.showIcon(IconNames.Happy)
 radio.setGroup(1)
 EM_IR.IrRemote_init(IrPins.P5)
